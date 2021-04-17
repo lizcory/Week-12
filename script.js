a6b91191e453d225d6e9358ef470c309b8da7773
lizcory <evcory96@gmail.com>
2021-04-16 20:32:21 -0400

    classwork

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const margin = {t: 50, r:50, b: 50, l: 50};
 const size = {w: 800, h: 800};
+const fontSize = {nodes: 10};
 const svg = d3.select('svg');
 
 size.w = document.querySelector('div#svg-container').clientWidth;
@@ -18,7 +19,8 @@ d3.json('data/flare-processed.json')
     draw();
 });
 
-function draw(data) {
+function draw() {
+    // console.log(data);
     let hierarchy = d3.hierarchy(data);
 
     let bilinks = link2way(hierarchy);
@@ -26,14 +28,22 @@ function draw(data) {
         .size([2*Math.PI, d3.min([size.w, size.h])/2 - 100])
         (bilinks);
 
-    let node = svg.append('g')
+    // every append should be dependent on the data, make sure only 1 group in svg
+    let node = containerG
+        .selectAll("g.nodes") // selecting groups with class nodes
+        .data([ layout.leaves() ])
+        .join('g')
+        .classed("nodes",true)
         .attr('font-family', 'sans-serif')
-        .attr('font-size', 10)
-        .selectAll('g')
-        .data(layout.leaves())
+        .attr('font-size', fontSize.nodes)
+        .selectAll('g') // selecting groups within g.nodes
+        .data(d => d)
         .join('g')
         .attr('transform', d => `rotate(${d.x * 180 / Math.PI - 90}) translate(${d.y},0)`)
-        .append('text')
+        // .append('text')
+        .selectAll("text")  // selecting text within groups within g.nodes
+        .data(d => [d])
+        .join("text")
         .attr('text-anchor', d => d.x < Math.PI ? 'start' : 'end')
         .attr('transform', d => d.x >= Math.PI ? 'rotate(180)' : null)
         .text(d => d.data.name);
@@ -43,7 +53,11 @@ function draw(data) {
         .radius(d => d.y)
         .angle(d => d.x);
 
-    let link = svg.append('g')
+    
+    let link = containerG.selectAll('g.links')
+        .data([100])
+        .join('g')
+        .classed("links", true)
         .attr('stroke', '#aaa')
         .attr('fill', 'none')
         .selectAll('path')
@@ -78,4 +92,38 @@ function link2way(root) {
 
 function id(node) {
     return `${node.parent ? id(node.parent) + '.' : ''}${node.data.name}`;
+}
+
+window.onresize = function() {
+    // console.log("resized");
+
+    //
+    size.w = document.querySelector('div#svg-container').clientWidth;
+    size.h = window.innerHeight * 0.8;
+
+    // 3:2
+    size.w = document.querySelector('div#svg-container').clientWidth;
+    size.h = size.w * (3/2);
+
+    // resize svg and everything in it
+    svg.attr('width', size.w)
+        .attr('height', size.h);
+
+    
+    // set new font size
+    if (size.w > 960) {
+        fontSize.nodes = 10;
+
+    } else if (size.w > 720) {
+        fontSize.nodes = 8;
+
+    } else if (size.w > 540) {
+        fontSize.nodes = 7;
+        
+    } else {
+        fontSize.nodes = 6;
+    }
+
+    draw();
+
 }
